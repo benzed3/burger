a95a7f4995d03e718ed5d224e20bc7485a09c529
benzed3 <benzed3@gmail.com>
2019-05-09 12:50:17 -0400

    added files

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("big mac", true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("whopper", false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("big boy", true);
+
+
