a667ae1f33f6790e801ec8eda0bde83de68eebb3
benzed3 <benzed3@gmail.com>
2019-05-13 17:33:05 -0400

    added code

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,34 @@
-var connection = require("./connection.js");
+var connection = require("../config/connection.js");
 
-var orm;
+var orm = {
+    all: function (tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+            console.log(queryString);
+        });
+    },
+    update: function (table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+            console.log(result);
+        });
+    }
+};
 
 module.exports = orm;
--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,18 @@
 var orm = require("../config/orm.js");
 
+var burger = {
+    all: function (cb) {
+        orm.all("burger_name", function (res) {
+            cb(res);
+            console.log(res);
+        });
+    },
+    update: function (objColVals, condition, cb) {
+        orm.update("burger_name", objColVals, condition, function (res) {
+            cb(res);
+        });
+    },
+};
 
-//module.exports = burger;
+
+module.exports = burger;
